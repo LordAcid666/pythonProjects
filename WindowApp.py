59ec27e169c6df830631f1dad97db2081b875459
Casalnovo Giacomo <gc76@live.it>
2023-03-03 15:23:33 +0100

    add button 2

--- WindowApp.py
+++ WindowApp.py
@@ -1,6 +1,7 @@
 # https://www.youtube.com/playlist?list=PLFhH2Ohz-yTHAxYzLZB8kJ3V5RPqsiXnN
 
 import tkinter as tk
+from tkinter.font import BOLD, ITALIC
 
 # creiamo la finestra
 window = tk.Tk()
@@ -10,8 +11,15 @@ window.resizable(False, False)
 window.config(bg="#232323")
 
 # creaiamo i widget (scritte-bottoni-immagini)
-label_ciao = tk.Label(window, text="ciao", font=("Times", 16), bg="#232323", fg="white")
+label_ciao = tk.Label(window, text="ciao", font=("Times", 16, BOLD), bg="#232323", fg="white")
 label_ciao.grid(row = 1, column = 0, padx=350, pady=20)
+
+label_indicazione = tk.Label(window, text="Clikka il bottone qui sotto:", font=("Times", 16, ITALIC), bg="#232323", fg="white")
+label_indicazione.grid(row = 2, column = 0, padx=350, pady=20)
+
+bottone_cliccami = tk.Button(window, text="cliccami", bd = 5, width=20)
+bottone_cliccami.grid(row = 3, column = 0, padx=350, pady=20)
+
 
 # far partire la app 
 window.mainloop()
