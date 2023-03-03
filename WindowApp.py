4605cb38e82bc9c1bb8f76750e1a81d92a52728b
Casalnovo Giacomo <gc76@live.it>
2023-03-03 15:13:52 +0100

    add label

--- WindowApp.py
+++ WindowApp.py
@@ -2,11 +2,16 @@
 
 import tkinter as tk
 
+# creiamo la finestra
 window = tk.Tk()
 window.title("la mia app")
 window.geometry("900x500")
 window.resizable(False, False)
+window.config(bg="#232323")
 
+# creaiamo i widget (scritte-bottoni-immagini)
+label_ciao = tk.Label(window, text="ciao", font=("Times", 16), bg="#232323", fg="white")
+label_ciao.grid(row = 1, column = 0, padx=350, pady=20)
 
-
+# far partire la app 
 window.mainloop()
